4f96b8ae3f17ec027f4ddac2387638721dfb9487
Santiago Rueda Ortiz <srueda27@gmail.com>
2024-11-15 17:51:55 -0500

    Update js/config api invokeUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,12 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://mbqvuiinq2.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod', API Gateway after deploy
     }
 };
+
+// srueda327@gmail.com
+// zJ]%9u&qK9K.Bp&
+// Auth Token for Cognito API: eyJraWQiOiJSZWlSWXlHdFJWMFA4VzBXYTVCOGx2UVFPWXdoOGkrWVlyS3hsNG12M0trPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJlNDI4MzQ0OC0wMDExLTcwNWItMWZlNy00MTYzN2EwZjY0ZjIiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfMnp3ZzFXeDRrIiwiY29nbml0bzp1c2VybmFtZSI6InNydWVkYTI3LWF0LWdtYWlsLmNvbSIsIm9yaWdpbl9qdGkiOiJkMTYxY2I1ZS1lYzc3LTQzMDgtYmEzNy1lNGExMWNiM2RlYzMiLCJhdWQiOiJzcGVqdTNwN2sxcGU3dXY5czFxYWZkdTVhIiwiZXZlbnRfaWQiOiI0MWEyMDg4NC1iYzVlLTRiYmItYjQwYy0xM2U5ZTYxMmNiZDkiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTczMTcwMzYyMiwiZXhwIjoxNzMxNzA3MjIyLCJpYXQiOjE3MzE3MDM2MjIsImp0aSI6IjA5YWU4MTZhLTU3NGEtNGMxZC1hMjUyLWQzMjUwMzg4YTQ1YyIsImVtYWlsIjoic3J1ZWRhMjdAZ21haWwuY29tIn0.zLNbWYqpLifrW8r7oyiCfbiiYq59QDyVFeE663zYxCcsII1AailDwORVee2oj42RyIlIQHotLk2NWyPMXa_QwBwjCuSmspEDA0jmUdPFuEdCy3BCNMQWXSKO3da19pZm_fgWhWP2GojFHmAZM1bHUqfm2dROaYHXhqs2hrFSPGwFM2Eag8hOzMlvVjbAJ5X9-QMCoQakgZldbOvlhrCo98h0hIUt_PE7UG8r7X22J7UYkb76Bojq3LloWW_OBA8KitNQszLUf5LYntaUIDk-6FeCpMe4m-PhZC-AbI8BxKKe3zB9nbK0JWP-EnZQWAIxvqyZabxEIidNYG6NDsaIAw
+// https://webapp.serverlessworkshops.io/ 
+// https://aws.amazon.com/es/getting-started/hands-on/build-serverless-web-app-lambda-apigateway-s3-dynamodb-cognito/module-3/?refid=ac00e2de-8a41-4a1e-b3cd-eb4cc45abb02
